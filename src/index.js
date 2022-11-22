7adeae8062bcb43eb0c9d4c37997406fa133c459
Max-Morozynskyi <m0r0zin@ukr.net>
2022-11-22 23:27:59 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,122 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+// import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// let lightbox = new SimpleLightbox('.gallery a');
+
+const formRef = document.querySelector('.search-form');
+const galleryRef = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.load-more')
+
+const URL = 'https://pixabay.com/api/?key=31504710-bca348681cce76d75d9bac8c5';
+let keyGet = ``;
+let page = 1;
+const per_page = 40;
+
+formRef.addEventListener('input', onInput);
+formRef.addEventListener('submit', onSubmit);
+
+function onInput(evt) {
+  keyGet = `q=${evt.target.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}`;
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  page = 1;
+  galleryRef.innerHTML = '';
+  moreBtn.classList.add('visually-hidden');
+
+  const getNull = "q=&image_type=photo&orientation=horizontal&safesearch=true&per_page=40";
+  if (getNull === keyGet || '' === keyGet) {
+    emptyValue();
+    return ;
+  }
+
+  fetchCard().then(data => {
+    if (data.length === 0) {
+      moreBtn.classList.add('visually-hidden');
+      nullArray();
+      return 
+    }
+
+    if (page >= 2) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+
+    renderGallery(data);
+    moreBtn.classList.remove('visually-hidden');
+    
+    if (data.length <= 40) {
+      moreBtn.classList.add('visually-hidden');
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  });
+}
+
+async function fetchCard() {
+  try {
+    const dataResponse = await axios.get(`${URL}&${keyGet}&page=${page}`);
+    return dataResponse.data;
+  } catch (error) {
+    console.log('ERROR: ', console.log(error));
+  }
+}
+
+function renderGallery(data) {
+  let markup = data.hits
+    .map(itm => {
+      return `<div class="photo-card">
+        <a class="gallery-item" href="${itm.largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${itm.webformatURL}"
+            alt="${itm.tags}"
+            loading="lazy"
+        /></a>
+        <div class="info">
+          <div class="info-box">
+            <p class="info-item">
+              <b>Likes</b>
+            </p>
+            <p class="info-counter">${itm.likes.toLocaleString()}</p>
+          </div>
+          <div class="info-box">
+            <p class="info-item">
+              <b>Views</b>
+            </p>
+            <p class="info-counter">${itm.views.toLocaleString()}</p>
+          </div>
+          <div class="info-box">
+            <p class="info-item">
+              <b>Comments</b>
+            </p>
+            <p class="info-counter">${itm.comments.toLocaleString()}</p>
+          </div>
+          <div class="info-box">
+            <p class="info-item">
+              <b>Downloads</b>
+            </p>
+            <p class="info-counter">${itm.downloads.toLocaleString()}</p>
+          </div>
+        </div>
+      </div>`;
+    })
+    .join('');
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+}
+
+function incrementPage() {
+  this.page += 1;
+}
+
+const nullArray = () => {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+  cardRef.innerHTML = '';
+};
+
+const emptyValue = () => {
+  Notiflix.Notify.warning('Go on! Enter your word!');
+  cardRef.innerHTML = '';
+};
